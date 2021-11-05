76de5705eca96d607f1e7d7767aeb310f688c970
unknown <victoria.brami88@gmail.com>
2021-11-05 12:02:47 +0100

    rmove old folder

--- optuna/net_model.py
+++
@@ -1,6 +0,0 @@
-class NetConfig(object):
-    def __init__(self):
-        self.min_linear_layers = 3
-        self.max_linear_layers = 7
-        self.min_linear_unit_size = 32
-        self.max_linear_unit_size = 512
